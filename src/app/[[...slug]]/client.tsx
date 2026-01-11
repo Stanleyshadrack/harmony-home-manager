f33a81652df971421f4ffa00330fefbc9df08bdd
StanleyShadrackM <mandestanleys@gmail.com>
2026-01-11 06:31:52 +0300

    new changes

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic'
 
-const App = dynamic(() => import("./page"), {
+const App = dynamic(() => import("@/App"), {
   ssr: false,
   loading: () => (
     <div className="flex h-screen items-center justify-center text-muted-foreground">
